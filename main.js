e62dac8ac380766028ebbf6ea875ce31dd86bb40
Roniel <ronielabreu22@gmail.com>
2021-04-06 23:39:31 -0400

    adding files

---
+++ main.js
@@ -0,0 +1,17 @@
+// for the time being i am just going to pass in my github username until i am able 
+// to get it from the user input
+getRepo("paradox05")
+
+function getRepo(user) {
+
+    fetch = ("https://api.github.com/users/" + user + "/repos");
+
+}
+
+
+function generate() {
+    
+
+
+
+}
